012e65b3cc30b5a3d6c270193cfd67670102138b
Oleksii Bolotin <samkiral73@gmail.com>
2023-05-03 11:56:52 +0200

    add RestrictedRoute for /login and /register

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from 'components/Layout/Layout';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchCurrentUser } from 'redux/auth/authOperations';
+import RestrictedRoute from 'components/RestrictedRoute';
 
 import useAuth from 'hooks/useAuth';
 const Home = lazy(() => import('pages/Home'));
@@ -18,15 +19,30 @@ function App() {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
 
-  const { isRefreshing } = useAuth;
+  const { isRefreshing } = useAuth();
 
   return (
     !isRefreshing && (
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                component={<RegisterForm />}
+                redirectTo="/contacts"
+              />
+            }
+          />
+
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute component={<Login />} redirectTo="/contacts" />
+            }
+          />
+
           <Route path="/contacts" element={<Contacts />} />
         </Route>
       </Routes>
