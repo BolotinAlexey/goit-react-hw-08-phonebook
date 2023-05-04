6f372ff37a2eaf17a1fb52e3e29c07ba0f53b259
Oleksii Bolotin <samkiral73@gmail.com>
2023-05-04 15:43:39 +0200

    decomposite in auth

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -2,6 +2,10 @@ import { createSlice } from '@reduxjs/toolkit';
 import Notiflix from 'notiflix';
 import { register, logIn, logOut, fetchCurrentUser } from './authOperations';
 
+const handleRejected = (_, action) => {
+  Notiflix.Notify.warning(action.payload);
+};
+
 const initialState = {
   user: { name: null, email: null },
   token: null,
@@ -18,26 +22,20 @@ const authSlice = createSlice({
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
-    [register.rejected](_, action) {
-      Notiflix.Notify.warning(action.payload);
-    },
+    [register.rejected]: handleRejected,
 
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
-    [logIn.rejected](_, action) {
-      Notiflix.Notify.warning(action.payload);
-    },
+    [logIn.rejected]: handleRejected,
     [logOut.fulfilled](state) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
     },
-    [logOut.rejected](_, action) {
-      Notiflix.Notify.warning(action.payload);
-    },
+    [logOut.rejected]: handleRejected,
     [fetchCurrentUser.pending](state) {
       state.isFetchingCurrentUser = true;
     },
